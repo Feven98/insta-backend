11ed3082b9d85a0edcd5af1e2c5cf80437d7b131
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-01-12 03:13:21 -0500

    creating user phrase

--- models/User.js
+++ models/User.js
@@ -14,7 +14,16 @@ const userSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
+    id: false,
+    toJSON: {
+        virtuals: true,
+
+        transform: (_doc, ret) => {
+            delete ret.password
+            return ret
+        }
   }
+}
 );
 
 module.exports = mongoose.model("User", userSchema);
