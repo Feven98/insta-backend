4b075893b0f6d2f6d57274ece1eb55e47af7b576
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-01-11 22:27:14 -0500

    create index.js

---
+++ models/Post.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+// schema
+const PostSchema = new mongoose.Schema({
+  image: {type: String, required: true},
+    caption: String,
+    location: String,
+    // comment: String,
+    // owner:{
+    //     type: mongoose.Schema.Types.ObjectId,
+    //   ref: 'Home',
+    //   required: true
+    // }
+}, {timespan: true})
+
+const Post = mongoose.model("Post", PostSchema)
+
+module.exports = Post
