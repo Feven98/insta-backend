28433f54325574de1c7a51c3d20aea6aac9b99dc
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-01-12 04:05:03 -0500

    correct some code

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -13,23 +13,22 @@ try{
     const salt = await bcrypt.genSalt(10)
 
     const passwordHash = await bcrypt.hash(req.body.password, salt)
-    // const rawPWStore = req.body.password
+    const rawPWStore = req.body.password
 
     req.body.password =  passwordHash
 
     const newUser = await User.create(req.body)
-    // if(newUser){ 
-    //     req.body.password = rawPWStore
-    //     const authenticatedUserToken = createUserToken(req, newUser);
+    if(newUser){ 
+        req.body.password = rawPWStore
+        const authenticatedUserToken = createUserToken(req, newUser);
     res.status(201).json({
         user: newUser, 
         isLoggedIn: true, 
-    //     token: authenticatedUserToken })
-    // } else{
-    //     res.status(400).json({error: "Something went wrong"})
-    })
-    // console.log(req.body.password)
-    // res.status(200).send(passwordHash)
+        token: authenticatedUserToken })
+    } else{
+        res.status(400).json({error: "Something went wrong"})
+    }
+
 }catch(err){
     res.status(400).json({err: err.message})
 }
@@ -44,7 +43,7 @@ router.post("/login", async (req, res, next) => {
       const foundUser = await User.findOne({ username: loggingUser });
       const token = await createUserToken(req, foundUser);
       res.status(200).json({
-        home: foundUser,
+        user: foundUser,
         isLoggedIn: true,
         token,
       });
