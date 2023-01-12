75b0c1b4498eb6eb85432c0050fe7a1b4ecd820c
Feven Seifu <fevenmenelik@Fevens-MacBook-Pro.local>
2023-01-12 01:48:24 -0500

    create user schema

---
+++ models/Comment.js
@@ -0,0 +1,22 @@
+// import mongoose
+
+const mongoose = require('mongoose')
+
+// create a schema variable/ object
+const CommentSchema = new mongoose.Schema({
+   comment: {type: String, required: [true, "need an comment"]},
+   body: String,
+//    person: {
+//     type: mongoose.Types.ObjectId, ref: "People"
+//    }
+
+},  {timestamps: true})
+
+
+
+// create a model w/ collection identifier
+const Comment = mongoose.model("Comment", CommentSchema)
+
+
+// export it 
+module.exports = Comment
